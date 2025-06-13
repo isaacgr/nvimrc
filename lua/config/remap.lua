932a2a9b10c6e97364f90d14790c8535f9e7a2c5
irowell <irowell@evertz.com>
2025-06-13 05:42:44 -0400

    Comment sets and remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,47 +1,63 @@
+-- Open netrw
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, {})
 
+-- move highlighted line/block up/down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 
+-- Join the current line with the next but keep the cursor in place
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- Half-page jump up/down keeping cursor centered
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
+-- Search next/previous result and center on the screen
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>vwm", function()
-    require("vim-with-me").StartVimWithMe()
-end
-)
-
-vim.keymap.set("n", "<leader>svwm", function()
-    require("vim-with-me").StopVimWithMe()
-end
-)
-
+-- Paste over selection without overwriting the default register
+-- This one is very good
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
+-- Yank whole line to system clipboard
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("v", "<leader>Y", "\"+Y")
 
+-- Delete without affecting the default register
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
+-- Disable annoying Ex mode key
 vim.keymap.set("n", "Q", "<nop>")
+
+-- Launch tmux-sessionizer in a new tmux window
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- Format buffer using LSP
 vim.keymap.set("n", "<leader>f", function()
     vim.lsp.buf.format()
 end
 )
 
+-- Jump to next/previous quickfix item and keep centered on screen
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+
+-- Jump to next/previous location list item and keep centered on screen
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- Search and replace word under the cursor globally, with confirmation
 vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
+
+-- Make the current file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {silent = true})
 
+-- Remove windows style carriage returns (^M)
 vim.keymap.set("n", "<leader>rm", ":%s/\r//g")
+
+-- Pretty print JSON file in current buffer using python
+vim.keymap.set("n", "<leader>J", ":%!python3 -m json.tool<CR>")
--- lua/config/set.lua
+++ lua/config/set.lua
@@ -8,23 +8,33 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+-- Disable swap file creation (for fewer temporary files)
 vim.opt.swapfile = false
 
+-- Directory to store undo history files for larger history
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
+-- Do not highlight matches after search
 vim.opt.hlsearch = false
+
+-- Show matches while typing a search query
 vim.opt.incsearch = true
 
+-- True color support in the terminal
 vim.opt.termguicolors = true
 
+-- Keep 8 lines of context above and below the cursor when scrolling
 vim.opt.scrolloff = 8
+
+-- Always show the sign column (for git signs etc.)
 vim.opt.signcolumn = "yes"
+
+-- Allow '@' as a valid character in filenames
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
