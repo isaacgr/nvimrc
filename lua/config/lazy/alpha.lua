4711395dc25968b46003ea480ffd3ced07d803a4
irowell <irowell@evertz.com>
2025-06-17 21:48:11 -0400

    Update alpha theme

--- lua/config/lazy/alpha.lua
+++ lua/config/lazy/alpha.lua
@@ -1,9 +1,57 @@
 return {
     {
         'goolord/alpha-nvim',
-        dependencies = { 'echasnovski/mini.icons' },
         config = function()
-            require 'alpha'.setup(require 'alpha.themes.startify'.config)
+            -- require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
+            -- Make sure alpha-nvim is installed via lazy.nvim or another plugin manager
+            local alpha = require("alpha")
+            local dashboard = require("alpha.themes.dashboard")
+            dashboard.section.header.val = {
+                "                                                                      ",
+                "                                                                      ",
+                "                                                                      ",
+                "                                    ▒░░░░░                             ",
+                "                            ░░▒    ░▒▒▒▒▒░░                            ",
+                "                        ░░░ ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒░                           ",
+                "                      ░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▒▒▒░ ░░                       ",
+                "                     ░░▒▒▒▒▒▒▒▒▒▒▓▒▒▒▒▒▓▓▓▓▒░░░░░░░                    ",
+                "                ░░░░░░▒▒▒▒▒▒▒▒▒▓▓▓▒▒▒▒██▓▓█▓▓▒▒░░░░                    ",
+                "               ░░░░░▒▒▒▒▒▒▒░▒▒▒▓▓▓▓▒▒▓██▓▓███▓▒▒░░░░░░░░               ",
+                "               ░▒░░▒▒▓▒▒▒░░░▒▒▒▓▓▓▓▓██▓▒▒▒▒▓▒▓▓▒▒▒░░░░░░░              ",
+                "              ░░▒░▒▒█▓▒▒▒░░░▒▒▒▒▒▓███▒▒▒▒▒▒░▒▒▓▒▒▒▒▒▒░▒▒░░            ",
+                "            ░░░░▒▒▒▓██▓▒▒▒▒▒▒▒▒▒░▒▒▓▓▒▒▒▓█▒▒▒░▒▒▓▓▒▒▒▒▒▒▒░░           ",
+                "           ░░░░▒▒▒▒▓██▓▓▒▒░░░░░░░░░░▒▒▒▒██▒▒▒░▒▒▓▓▒▒▒▒▓▓▒░░           ",
+                "           ░░░░▒▒▒▒▓███▓▒▒▒░░░░▒▒▒▒░░▒▒███▒▒▒▒▒▒▓▓▒▓▓▒▓▓▓░░           ",
+                "           ░░░▒▒▒▒▒▒▓█▓▒▒▒▒░▒▒░▓▒▒▒░▒▓▓▓██▓▒▓▒▒▒▒▓▓▓▓▓▓▓▓░░           ",
+                "           ░░▒▒▒▒▒▒▓▓▓▒▓▒▒▒▒▓▓▒▒▓▓▒▒▒▓▓▓▓█▓▓▓▓▒▒▓▓▓▓█▓▓▓▒░            ",
+                "            ░▒▒▒▒▓▓▓▓▓▓▓▓▒▓▓▓▓▒▒▓▓▓▓▒▓▓░░█▓▓▓▓░▒▒▒▓▓▓▒▒▒░             ",
+                "            ░▒▒░░▒▒▒░░▒▓▓▓▓▓▓▓▓▒▓███▒▒▓░▓▓▓▒▒▒░░▒▒▒░░░░░              ",
+                "             ░░░ ░▒░  ░▓▓▓▓▒▒▓▓▒▓██▓▓▒▓▓░░░░▒░ ░░░░ ░░░               ",
+                "               ░  ░░   ▒▓▓▒░▒▓▒▒▒███▒▒▓      ░ ░░░                   ",
+                "                  ░░     ░░ ░░░▒░████▒░        ░░                    ",
+                "                               ░░▓▓██░                                ",
+                "                                 ▓▓███                                ",
+                "                       ░░░▒▒▒▒▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒░                        ",
+                "                            ░▒▒░░░░░▒░▒░░                             ",
+                "                                                                      ",
+                "                                                                      ",
+            }
+            dashboard.section.buttons.val = {
+                dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+                dashboard.button("r", "  Recent files", ":Telescope oldfiles<CR>"),
+                dashboard.button("n", "  New file", ":ene <BAR> startinsert <CR>"),
+                dashboard.button("c", "  Config", ":e $MYVIMRC<CR>"),
+                dashboard.button("q", "  Quit", ":qa<CR>"),
+            }
+
+            dashboard.section.footer.val = "> wanting it to be easy "
+
+            -- Optional styling
+            dashboard.section.header.opts.hl = "Constant"
+            dashboard.section.buttons.opts.hl = "Function"
+            dashboard.section.footer.opts.hl = "Type"
+
+            alpha.setup(dashboard.config)
         end
-    },
+    }
 }
